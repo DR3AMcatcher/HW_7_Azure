05db7a002ed177f7b1a7a588176b80a2e0be0f12
DR3AMcatcher <ap@4teco.com>
2019-01-20 02:02:06 +0200

    1

---
+++ HomeWork7/SetUp.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace HomeWork7
+{
+    [SetUpFixture]
+    public class SetUpFixture
+    {
+        public IWebDriver driver;
+        
+        [OneTimeSetUp]
+        public void InitializeDriver()
+        {
+            var chromeOptions = new ChromeOptions();
+        
+            driver = new ChromeDriver(chromeOptions);
+        }
+
+        [OneTimeTearDown]
+        public void Clean()
+        {
+            driver.Quit();
+        }
+    }
+}
